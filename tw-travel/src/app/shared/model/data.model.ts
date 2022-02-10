ec509ad8afa338d9518759a2b01b8508aa1919d1
pfShi <pfshi@freedom-tek.com>
2022-02-10 08:42:01 +0800

    常數名稱修改

--- tw-travel/src/app/shared/model/data.model.ts
+++ tw-travel/src/app/shared/model/data.model.ts
@@ -21,23 +21,14 @@ export const CITYLIST = [
   '嘉義市',
 ];
 
-export const ATTRACTIONSLIST = [
-  { label: '觀光工廠類', src: '' },
-  { label: '文化類', src: '' },
-  { label: '遊憩類', src: '' },
-  { label: '體育健身類', src: '' },
-  { label: '都會公園類', src: '' },
-  { label: '自然風景類', src: '' },
-  { label: '國家風景區類', src: '' },
-  { label: '國家公園類', src: '' },
-  { label: '廟宇類', src: '' },
-  { label: '小吃/特產類', src: '' },
-  { label: '生態類', src: '' },
-  { label: '古蹟類', src: '' },
-  { label: '休閒農業類', src: '' },
-  { label: '藝術類', src: '' },
-  { label: '森林遊樂區類', src: '' },
-  { label: '林場類', src: '' },
+export const ACTIVITYLIST = [
+  { label: '活動快報', src: '' },
+  { label: '年度活動', src: '' },
+  { label: '節慶活動', src: '' },
+  { label: '藝文活動', src: '' },
+  { label: '四季活動', src: '' },
+  { label: '產業文化活動', src: '' },
+  { label: '遊憩活動', src: '' },
   { label: '其他', src: '' },
 ];
 
@@ -53,13 +44,22 @@ export const RESTAURANTSLIST = [
   { label: '其他', src: '' },
 ];
 
-export const ACTIVITYLIST = [
-  { label: '活動快報', src: '' },
-  { label: '年度活動', src: '' },
-  { label: '節慶活動', src: '' },
-  { label: '藝文活動', src: '' },
-  { label: '四季活動', src: '' },
-  { label: '產業文化活動', src: '' },
-  { label: '遊憩活動', src: '' },
+export const SCENICSPOT = [
+  { label: '觀光工廠', src: '' },
+  { label: '文化', src: '' },
+  { label: '遊憩', src: '' },
+  { label: '體育健身', src: '' },
+  { label: '都會公園', src: '' },
+  { label: '自然風景', src: '' },
+  { label: '國家風景區', src: '' },
+  { label: '國家公園', src: '' },
+  { label: '廟宇', src: '' },
+  { label: '小吃/特產', src: '' },
+  { label: '生態', src: '' },
+  { label: '古蹟', src: '' },
+  { label: '休閒農業', src: '' },
+  { label: '藝術', src: '' },
+  { label: '森林遊樂區', src: '' },
+  { label: '林場', src: '' },
   { label: '其他', src: '' },
 ];
