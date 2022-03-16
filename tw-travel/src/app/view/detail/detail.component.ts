09bedfd340607d79c7e9f40c92fe4c123ea53691
pfShi <pfshi@freedom-tek.com>
2022-03-16 15:38:58 +0800

    完善detail頁

--- tw-travel/src/app/view/detail/detail.component.ts
+++ tw-travel/src/app/view/detail/detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ItemsList } from '@ng-select/ng-select/lib/items-list';
+import { map, Observable, switchMap } from 'rxjs';
+import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
   selector: 'app-detail',
@@ -6,7 +10,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
 
-  ngOnInit(): void {}
+    private dataService: DataService
+  ) {}
+
+  name: string;
+
+  page: string;
+
+  detailData: any;
+
+  ngOnInit(): void {
+    let observable: Observable<Array<any>>;
+    this.route.queryParamMap
+      .pipe(
+        switchMap((queryParams: any) => {
+          const { page, name, theme } = queryParams.params;
+          this.page = page;
+          this.name = name;
+
+          if (page === 'Activity') {
+            observable = this.dataService.getActivity({ theme });
+          }
+
+          if (page === 'Restaurant') {
+            observable = this.dataService.getRestaurant({ theme });
+          }
+
+          if (page === 'ScenicSpot') {
+            observable = this.dataService.getScenicSpot({ theme });
+          }
+          return observable;
+        })
+      )
+      .subscribe((res) => {        console.log(res);
+
+        this.detailData = res
+          .filter((item) => item[`${this.page}Name`] === this.name)
+          .map((item) => ({
+            ...item,
+            Name: item[`${this.page}Name`],
+          }))[0];
+
+        console.log(this.detailData);
+      });
+  }
 }
