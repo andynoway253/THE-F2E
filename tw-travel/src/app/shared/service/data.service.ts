2f97cfb18cf2a0bb7f6aa2b9217791dd5b388eca
pfShi <pfshi@freedom-tek.com>
2022-04-22 15:41:58 +0800

    修改service url

--- tw-travel/src/app/shared/service/data.service.ts
+++ tw-travel/src/app/shared/service/data.service.ts
@@ -1,54 +1,61 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ItemsList } from '@ng-select/ng-select/lib/items-list';
-import { BehaviorSubject, map, Observable } from 'rxjs';
-import { BaseService } from 'src/app/shared';
+import { Observable } from 'rxjs';
+import { BaseApi } from '../api/base.api';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DataService extends BaseService {
+export class DataService extends BaseApi {
   constructor(http: HttpClient) {
     super(http);
   }
 
   getScenicSpotAll(): Observable<Array<any>> {
-    return this.get('v2/Tourism/ScenicSpot?%24top=4&%24format=JSON');
+    return this.get('ScenicSpot?%24top=4');
   }
 
-  getScenicSpotByCity(params: { city: string }): Observable<Array<any>> {
-    return this.get('/v2/Tourism/ScenicSpot/' + `${params.city}`);
+  getScenicSpotByCity(params: { city: any }): Observable<Array<any>> {
+    return this.get('ScenicSpot/' + `${params.city}`);
   }
 
-  getScenicSpot(params: { theme: string }): Observable<Array<any>> {
-    return this.get(
-      `/v2/Tourism/ScenicSpot?$filter=Class1 eq '${params.theme}'`
-    );
+  getScenicSpot(params: { theme: any }): Observable<Array<any>> {
+    return this.get(`ScenicSpot?$filter=Class1 eq '${params.theme}類'`);
   }
 
   getRestaurantAll(): Observable<Array<any>> {
-    return this.get('/v2/Tourism/Restaurant?%24top=4&%24format=JSON');
+    return this.get('Restaurant?%24top=4');
   }
 
-  getRestaurantByCity(params: { city: string }): Observable<Array<any>> {
-    return this.get('/v2/Tourism/Restaurant/' + `${params.city}`);
+  getRestaurantByCity(params: { city: any }): Observable<Array<any>> {
+    return this.get('Restaurant/' + `${params.city}`);
   }
 
-  getRestaurant(params: { theme: string }): Observable<Array<any>> {
-    return this.get(
-      `/v2/Tourism/Restaurant?$filter=Class eq '${params.theme}'`
-    );
+  getRestaurant(params: { theme: any }): Observable<Array<any>> {
+    return this.get(`Restaurant?$filter=Class eq '${params.theme}'`);
   }
 
   getActivityAll(): Observable<Array<any>> {
-    return this.get('v2/Tourism/Activity?%24top=4&%24format=JSON');
+    return this.get('Activity?%24top=4');
   }
 
-  getActivityByCity(params: { city: string }): Observable<Array<any>> {
-    return this.get('/v2/Tourism/Activity/' + `${params.city}`);
+  getActivityByCity(params: {
+    city: any;
+    theme?: any;
+  }): Observable<Array<any>> {
+    if (params.theme) {
+      return this.get(
+        'Activity/' +
+          `${params.city}?` +
+          '$filter=Class1 eq ' +
+          `'${params.theme}'`
+      );
+    } else {
+      return this.get('Activity/' + `${params.city}`);
+    }
   }
 
-  getActivity(params: { theme: string }): Observable<Array<any>> {
-    return this.get(`/v2/Tourism/Activity?$filter=Class1 eq '${params.theme}'`);
+  getActivity(params: { theme: any }): Observable<Array<any>> {
+    return this.get(`Activity?$filter=Class1 eq '${params.theme}'`);
   }
 }
