c561e893ec30fcedf1d48ce438a449e5a28aaafa
pfShi <pfshi@freedom-tek.com>
2022-04-22 15:41:02 +0800

    修改url

--- tw-travel/src/app/shared/api/base.api.ts
+++ tw-travel/src/app/shared/api/base.api.ts
@@ -5,14 +5,14 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class BaseService {
+export class BaseApi {
   constructor(private http: HttpClient) {}
 
   // post(url: string): Observable<any> {
   //   return this.http.post<any>(url);
   // }
 
-  baseUrl = 'https://ptx.transportdata.tw/MOTC/';
+  baseUrl = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/';
 
   get(url: string): Observable<any> {
     return this.http.get<any>(this.baseUrl + url);
