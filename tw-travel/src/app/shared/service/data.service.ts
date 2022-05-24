0962f2be4b5c9b9ba35ecb7780fedd0fb2a4a971
pfShi <pfshi@freedom-tek.com>
2022-05-24 10:53:10 +0800

    精簡api

--- tw-travel/src/app/shared/service/data.service.ts
+++ tw-travel/src/app/shared/service/data.service.ts
@@ -11,51 +11,37 @@ export class DataService extends BaseApi {
     super(http);
   }
 
-  getScenicSpotAll(): Observable<Array<any>> {
-    return this.get('ScenicSpot?%24top=4');
-  }
-
-  getScenicSpotByCity(params: { city: any }): Observable<Array<any>> {
-    return this.get('ScenicSpot/' + `${params.city}`);
+  getDataAll(params: { category: string }): Observable<Array<any>> {
+    return this.get(`${params.category}?%24top=4`);
   }
 
   getScenicSpot(params: { theme: any }): Observable<Array<any>> {
-    return this.get(`ScenicSpot?$filter=Class1 eq '${params.theme}類'`);
-  }
-
-  getRestaurantAll(): Observable<Array<any>> {
-    return this.get('Restaurant?%24top=4');
-  }
-
-  getRestaurantByCity(params: { city: any }): Observable<Array<any>> {
-    return this.get('Restaurant/' + `${params.city}`);
+    return this.get(`ScenicSpot?$filter=Class1 eq '${params.theme}'`);
   }
 
   getRestaurant(params: { theme: any }): Observable<Array<any>> {
     return this.get(`Restaurant?$filter=Class eq '${params.theme}'`);
   }
 
-  getActivityAll(): Observable<Array<any>> {
-    return this.get('Activity?%24top=4');
-  }
-
-  getActivityByCity(params: {
-    city: any;
-    theme?: any;
-  }): Observable<Array<any>> {
-    if (params.theme) {
-      return this.get(
-        'Activity/' +
-          `${params.city}?` +
-          '$filter=Class1 eq ' +
-          `'${params.theme}'`
-      );
-    } else {
-      return this.get('Activity/' + `${params.city}`);
-    }
-  }
-
   getActivity(params: { theme: any }): Observable<Array<any>> {
     return this.get(`Activity?$filter=Class1 eq '${params.theme}'`);
+  }
+
+  // getData(params: { category: string; theme: string }) {
+  //   const { category, theme } = params;
+
+  //   return this.get(`${category}?$filter=Class1 eq '${theme}'`);
+  // }
+
+  getDataByCity(params: { category: string; city: any; theme?: any | null }) {
+    const { category, city, theme } = params;
+
+    if (theme) {
+      return this.get(
+        `${category}/` + `${city}?$filter=Class1 eq ` + `'${theme}'`
+      );
+    } else {
+      return this.get(`${category}/` + `${city}`);
+    }
   }
 }
