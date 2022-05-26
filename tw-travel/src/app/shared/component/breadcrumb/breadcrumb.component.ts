90343169c74d67da328864e433ee0da6551a3bf0
pfShi <pfshi@freedom-tek.com>
2022-05-26 10:57:15 +0800

    刪除不必要引入

--- tw-travel/src/app/shared/component/breadcrumb/breadcrumb.component.ts
+++ tw-travel/src/app/shared/component/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,6 @@
 import { Subscription, switchMap } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from './breadcrumb.service';
 
 @Component({
